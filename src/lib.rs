1b283f57fd682dc4f665f4a216751a184870fba0
rafawo <rafawo1@hotmail.com>
2018-12-20 14:45:44 -0800

    Remove dependency to libc crate

--- src/lib.rs
+++ src/lib.rs
@@ -19,52 +19,26 @@ pub(crate) mod hypervdevicevirtualization_bindings;
 
 #[allow(dead_code)]
 pub(crate) mod windefs {
-    use libc;
+    //! Defines type aliases for Windows Definitions to user Rust naming conventions
+    //! throughout the crate.
 
-    pub type Bool = libc::c_int;
-    pub type Boolean = libc::c_uchar;
-    pub type Byte = libc::c_uchar;
-    pub type DWord = libc::c_ulong;
-    pub type Handle = *mut libc::c_void;
-    pub type HResult = libc::c_long;
-    pub type PCWStr = *const libc::wchar_t;
-    pub type PWStr = *mut libc::wchar_t;
-    pub type PSId = *mut Void;
-    pub type PVoid = *mut Void;
-    pub type SecurityDescriptorControl = libc::c_ushort;
-    pub type Word = libc::c_ushort;
-    pub type Void = libc::c_void;
+    pub type Bool = winapi::shared::minwindef::BOOL;
+    pub type Boolean = winapi::shared::ntdef::BOOLEAN;
+    pub type Byte = winapi::shared::minwindef::BYTE;
+    pub type DWord = winapi::shared::minwindef::DWORD;
+    pub type Handle = winapi::shared::ntdef::HANDLE;
+    pub type HResult = winapi::shared::ntdef::HRESULT;
+    pub type PCWStr = winapi::shared::ntdef::PCWSTR;
+    pub type PWStr = winapi::shared::ntdef::PWSTR;
+    pub type PVoid = winapi::shared::ntdef::PVOID;
+    pub type UChar = winapi::shared::ntdef::UCHAR;
+    pub type Void = winapi::shared::ntdef::VOID;
+    pub type WChar = winapi::shared::ntdef::WCHAR;
+    pub type Word = winapi::shared::minwindef::WORD;
 
-    #[repr(C)]
-    #[derive(Debug, Copy, Clone)]
-    pub struct Guid {
-        pub data1: libc::c_ulong,
-        pub data2: libc::c_ushort,
-        pub data3: libc::c_ushort,
-        pub data4: [libc::c_uchar; 8],
-    }
-
-    #[repr(C)]
-    #[derive(Debug, Copy, Clone)]
-    pub struct Acl {
-        pub acl_revision: Byte,
-        pub sbz1: Byte,
-        pub acl_size: Word,
-        pub ace_count: Word,
-        pub sbz2: Word,
-    }
-
-    #[repr(C)]
-    #[derive(Debug, Copy, Clone)]
-    pub struct SecurityDescriptor {
-        pub revision: Byte,
-        pub sbz1: Byte,
-        pub control: SecurityDescriptorControl,
-        pub owner: PSId,
-        pub group: PSId,
-        pub sacl: *mut Acl,
-        pub dacl: *mut Acl,
-    }
+    pub type Guid = winapi::shared::guiddef::GUID;
+    pub type Acl = winapi::um::winnt::ACL;
+    pub type SecurityDescriptor = winapi::um::winnt::SECURITY_DESCRIPTOR;
 }
 
 /// Common result codes that can be returned by the HCS APIs
