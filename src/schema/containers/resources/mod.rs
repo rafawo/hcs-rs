0dfd31027602d58e09a044cd5a1cf55a3121d0c9
rafawo <rafawo1@hotmail.com>
2019-07-31 17:20:22 -0700

    Run cargo fmt

--- src/schema/containers/resources/mod.rs
+++ src/schema/containers/resources/mod.rs
@@ -5,4 +5,3 @@
 // All files in the project carrying such notice may not be copied, modified, or distributed
 // except according to those terms.
 // THE SOURCE CODE IS AVAILABLE UNDER THE ABOVE CHOSEN LICENSE "AS IS", WITH NO WARRANTIES.
-
