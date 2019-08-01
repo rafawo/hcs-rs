ac499ef92503a51412a7ead9c578ab0c4f31b3a3
rafawo <rafawo1@hotmail.com>
2019-07-31 17:14:54 -0700

    Add GUID serde deserialize and serialize helpers

--- src/schema/mod.rs
+++ src/schema/mod.rs
@@ -20,3 +20,103 @@ pub mod registry;
 pub mod requests;
 pub mod responses;
 pub mod virtual_machines;
+
+pub mod guid_serde {
+    use winutils_rs::windefs::Guid;
+
+    pub fn serialize<S>(guid: &Guid, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::ser::Serializer,
+    {
+        let guid_string = format!(
+            "{:08x}-{:04x}-{:04x}-{:02x}{:02x}-{:02x}{:02x}{:02x}{:02x}{:02x}{:02x}",
+            guid.Data1,
+            guid.Data2,
+            guid.Data3,
+            guid.Data4[0],
+            guid.Data4[1],
+            guid.Data4[2],
+            guid.Data4[3],
+            guid.Data4[4],
+            guid.Data4[5],
+            guid.Data4[6],
+            guid.Data4[7],
+        );
+        serializer.serialize_str(&guid_string)
+    }
+
+    pub fn deserialize<'de, D>(deserializer: D) -> Result<Guid, D::Error>
+    where
+        D: serde::de::Deserializer<'de>,
+    {
+        use serde::de::Deserialize;
+        let string_guid = String::deserialize(deserializer)?;
+        Ok(winutils_rs::utilities::parse_guid(&string_guid)
+            .expect(&format!("Failed to parse guid {}", &string_guid)))
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use winutils_rs::windefs::Guid;
+        use winutils_rs::windefs::GUID_NULL;
+
+        #[derive(serde::Serialize, serde::Deserialize)]
+        struct GuidWrapper {
+            #[serde(with = "crate::schema::guid_serde")]
+            guid: Guid,
+        }
+
+        const GUID_TEST: Guid = Guid {
+            Data1: 0xdb20fa3e,
+            Data2: 0xc476,
+            Data3: 0x447f,
+            Data4: [0x94, 0xa5, 0x51, 0xb8, 0x32, 0x2c, 0x4c, 0x4f],
+        };
+
+        macro_rules! guid_null_string {
+            () => {
+                r#"{"guid":"00000000-0000-0000-0000-000000000000"}"#
+            };
+        }
+
+        macro_rules! guid_test_string {
+            () => {
+                r#"{"guid":"db20fa3e-c476-447f-94a5-51b8322c4c4f"}"#
+            };
+        }
+
+        #[test]
+        fn guid_null_to_string() {
+            assert_eq!(
+                &serde_json::to_string(&GuidWrapper { guid: GUID_NULL }).unwrap(),
+                guid_null_string!()
+            );
+        }
+
+        #[test]
+        fn string_to_guid_null() {
+            let wrapper: GuidWrapper = serde_json::from_str(guid_null_string!()).unwrap();
+            assert!(winutils_rs::utilities::guid_are_equal(
+                &wrapper.guid,
+                &GUID_NULL
+            ));
+        }
+
+        #[test]
+        fn guid_to_string() {
+            assert_eq!(
+                &serde_json::to_string(&GuidWrapper { guid: GUID_TEST }).unwrap(),
+                guid_test_string!()
+            );
+        }
+
+        #[test]
+        fn string_to_guid() {
+            let wrapper: GuidWrapper = serde_json::from_str(guid_test_string!()).unwrap();
+            assert!(winutils_rs::utilities::guid_are_equal(
+                &wrapper.guid,
+                &GUID_TEST
+            ));
+        }
+    }
+}
